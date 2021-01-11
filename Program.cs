7050839a8e97ee1097bc9f9369b44a953ac29ed1
Jerry <jeremiasz.mozgwa@jazzy.pro>
2021-01-11 09:56:05 +0100

    ported to 5.0

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace BlazorApp
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services
                 .AddScoped<IAuthenticationService, AuthenticationService>()
